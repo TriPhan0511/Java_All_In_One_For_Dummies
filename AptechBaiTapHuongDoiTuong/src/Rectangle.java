c3a501645a9f6073628c009d0837292d2593a90b
Tri Phan <phanphutri@gmail.com>
2021-10-31 22:17:57 +0700

    Updated

---
+++ AptechBaiTapHuongDoiTuong/src/Rectangle.java
@@ -0,0 +1,68 @@
+// A class imitate a rectangle.
+// This class consists of length, width of a rectangle.
+// It also includes two methods which calculate the rectangle's area and perimeter.
+class Rectangle 
+{
+//	Private fields
+	private double length;
+	private double width;
+	
+//	Default constructor
+	public Rectangle() {}
+	
+//	Another constructor
+	public Rectangle(double length, double width) 
+			throws Exception
+		
+	{
+		if (length <= 0 || width <= 0)
+		{
+			throw new Exception();
+		}
+		this.length = length;
+		this.width = width;
+	}
+	
+//	Getters / Setters
+	public double getLength()
+	{
+		return this.length;
+	}
+	
+	public double getWidth()
+	{
+		return this.width;
+	}
+	
+	public void setLength(double length) 
+			throws Exception
+	{
+		if (length <= 0)
+		{
+			throw new Exception();
+		}
+		this.length = length;
+	}
+	
+	public void setWidth(double width) 
+			throws Exception
+	{
+		if (width <= 0)
+		{
+			throw new Exception();
+		}
+		this.width = width;
+	}
+	
+//	calcArea method: calculate the area of a rectangle
+	public double calcArea()
+	{
+		return this.length * this.width;
+	}
+	
+//	calcPerimeter method: calculate the perimeter of a rectangle
+	public double calcPerimeter()
+	{
+		return 2 * (this.length + this.width);
+	}
+}
