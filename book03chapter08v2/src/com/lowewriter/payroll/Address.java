bf209f82f91bc3b5c31a2c93c6cda1c946be6836
Tri Phan <phanphutri@gmail.com>
2021-12-26 07:09:12 +0700

    Updated

---
+++ book03chapter08v2/src/com/lowewriter/payroll/Address.java
@@ -0,0 +1,14 @@
+package com.lowewriter.payroll;
+
+/**
+ * Respresents an address.
+ * @author Doug Lowe.
+ *
+ */
+public class Address implements Cloneable 
+{
+	public String street;
+	public String city;
+	public String state;
+	public String zipCode;
+}
